bea27ca27c6ef61b4d075c0d28acc1ee2f498c34
Thomas Buck <taucher.bodensee@googlemail.com>
2012-08-13 02:15:32 +0200

    Changed license.

--- include/time.h
+++ include/time.h
@@ -1,22 +1,22 @@
 /*
  * time.h
  *
- * Copyright 2011 Thomas Buck <xythobuz@me.com>
+ * Copyright 2012 Thomas Buck <xythobuz@me.com>
  *
- * This file is part of xyRobot.
+ * This file is part of avrNetStack.
  *
- * xyRobot is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * avrNetStack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * xyRobot is distributed in the hope that it will be useful,
+ * avrNetStack is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with xyRobot.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with avrNetStack.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef _time_h
 #define _time_h
